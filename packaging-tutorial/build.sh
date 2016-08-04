52ccd1a0d62e68a607d636601c118a40b62f4e93
Izharul Haq <atoz.chevara@yahoo.com>
2016-08-04 18:49:49 +0700

    update build.sh

--- packaging-tutorial/build.sh
+++ packaging-tutorial/build.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+#
+# Author: Izharul Haq <atoz.chevara [at] yahoo [dot] com>
+# Usage: ./build.sh <lang>
+# Version: 1.0
+
+set -e
+
+LANG=$1
+
+if ! [[ "${LANG}" != "" && "${LANG}" =~ ^[^abc]*$ ]]
+then
+    echo "Usage: ./build.sh <lang>" >&2
+    echo "ex: ./build.sh id"
+    exit 1
+fi
+
 po4a po4a/po4a.cfg --verbose
-pdflatex packaging-tutorial.id.tex
+pdflatex packaging-tutorial.$LANG.tex
+cp packaging-tutorial.$LANG.tex{,.bak}
+cp packaging-tutorial.$LANG.toc{,.bak}
+rm debiantutorial.*.sty
+mv packaging-tutorial.$LANG.pdf pdf/
